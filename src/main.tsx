48463c7b818c565d90636625cfe06c573018779c
juanploaizan <juanp.loaizan@uqvirtual.edu.co>
2023-05-27 16:06:15 -0500

    base updated

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import './satoshi.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Router>
+    <Router basename='/bernie_app'>
       <App />
     </Router>
   </React.StrictMode>,
